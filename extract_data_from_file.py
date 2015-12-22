a33f347b422c63db2d0a2076ac45ca6973a0f47e
Hui <guohui@umich.edu>
2015-12-22 16:39:46 -0500

    new id_rsa

--- extract_data_from_file.py
+++ extract_data_from_file.py
@@ -3,56 +3,59 @@
 
 from os import listdir
 from os.path import isfile, join
+import sys
 
-mypath = '.'
+def analysis_data_in_dir(mypath):
 
-files = [f for f in listdir(mypath) if isfile(join(mypath,f))]
-for f in files:
-    print join(mypath,f)
-exit(0)
+    files = [f for f in listdir(mypath) if isfile(join(mypath,f))]
 
-sets_data = list()
-gets_data = list()
+    sets_data = list()
+    gets_data = list()
 
-for f in files:
-    data_file = open(f,"r")
+    for f in files:
+        data_file = open(join(mypath,f),"r")
+        obj_size = str()
+        if(f.startswith('1')):
+            obj_size = f.split('-')[5]
+        else:
+            continue
 
-    for l in data_file.readlines():
-        l = l.strip()
-        if l.endswith("Threads"):
-            splited_l = l.split()
-            sets_data.append(splited_l[0])
-            sets_data.append(',')
-            gets_data.append(splited_l[0])
-            gets_data.append(',')
-        if l.endswith("thread"):
-            splited_l = l.split()
-            sets_data.append(splited_l[0])
-            sets_data.append(',')
-            gets_data.append(splited_l[0])
-            gets_data.append(',')
-        if l.startswith("Sets"):
-            splited_l = l.split()
-            if splited_l[5].startswith("0.00"):
-                print f
-                del sets_data[-6:]
-                del gets_data[-6:]
-                continue
-            sets_data.append(','.join([splited_l[1],splited_l[4],splited_l[5]]))
-            sets_data.append("\n")
-        if l.startswith("Gets"):
-            splited_l = l.split()
-            gets_data.append(','.join([w for w in splited_l if not w.startswith('G')]))
-            gets_data.append("\n")
-    data_file.close()
+        for l in data_file.readlines():
+            l = l.strip()
+            if l.endswith("Threads"):
+                sets_data.append(obj_size)
+                sets_data.append(',')
+                gets_data.append(obj_size)
+                gets_data.append(',')
+                splited_l = l.split()
+                sets_data.append(splited_l[0])
+                sets_data.append(',')
+                gets_data.append(splited_l[0])
+                gets_data.append(',')
+            if l.endswith("thread"):
+                splited_l = l.split()
+                sets_data.append(splited_l[0])
+                sets_data.append(',')
+                gets_data.append(splited_l[0])
+                gets_data.append(',')
+            if l.startswith("Sets"):
+                splited_l = l.split()
+                sets_data.append(','.join([splited_l[1],splited_l[4],splited_l[5]]))
+                sets_data.append("\n")
+            if l.startswith("Gets"):
+                splited_l = l.split()
+                gets_data.append(','.join([w for w in splited_l if not w.startswith('G')]))
+                gets_data.append("\n")
+        data_file.close()
 
-#print sets_data
+    f = open("set",'w')
+    f.writelines(sets_data)
+    f.close()
 
+    f = open("get",'w')
+    f.writelines(gets_data)
+    f.close()
 
-f = open("set",'w')
-f.writelines(sets_data)
-f.close()
+if __name__ == "__main__":
+    analysis_data_in_dir(sys.argv[1])
 
-f = open("get",'w')
-f.writelines(gets_data)
-f.close()
